7cf3aecf9d12c45f073ac574c62895a860322590
Emily-Philbrick <63964311+Emily-Philbrick@users.noreply.github.com>
2020-05-04 13:32:15 -0600

    Add files via upload

---
+++ Basic Javascript projects/JS/Basic_Javascript_1.js
@@ -0,0 +1,29 @@
+
+var A = "Hello";
+window.alert(A); 
+
+
+document.write('Hello,world!');
+
+
+document.write("Lisa told Bart, \"Knock it off, Bart!");
+
+var str = "Hello World!";
+var result = str.fontcolor("green");
+
+document.write("\Be who you are and say what you feel,"
++"because those who mind don\'t mattter.\""
++"-Dr. Seuss");
+
+
+var blues ="I said" + "Step aside"
+var blues = blues.fontcolor("blue");
+document.write(blues);
+   
+
+var Family= "the Arezzinis", Dad= "Jeremiah", mom="hermoine",
+    daughter= "penny", son="Zorro";
+document.write(Dad);
+
+document.write("<p>Fontcolor: " + txt.fontcolor("green") + "</p>");
+
