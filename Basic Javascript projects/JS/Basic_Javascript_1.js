7a200610b291296397dce057296c07d4434e4953
Emily-Philbrick <63964311+Emily-Philbrick@users.noreply.github.com>
2020-05-06 09:05:53 -0600

    Add files via upload
    My """""<p id="Test" onclick="my_Function()"></p>""""    function is not working 
    
    please help

--- Basic Javascript projects/JS/Basic_Javascript_1.js
+++ Basic Javascript projects/JS/Basic_Javascript_1.js
@@ -1,33 +1,32 @@
 
-var A = "Hello";
-window.alert(A); 
-
 
 document.write('Hello,world!');
 
 
+var A = "Hello";
+window.alert(A); 
+
+
+
 document.write("Lisa told Bart, \"Knock it off, Bart!");
 
-var str = "Hello World!";
-var result = str.fontcolor("green");
+
 
 document.write("\Be who you are and say what you feel,"
 +"because those who mind don\'t mattter.\""
 +"-Dr. Seuss");
 
 
-var blues ="I said" + "Step aside"
-var blues = blues.fontcolor("blue");
-document.write(blues);
-   
+
+
+var B ="I said" + "Step aside"
+var blues = fontcolor("blue");
+document.write("B , blues"); 
+
 var Family= "the Arezzinis", Dad= "Jeremiah", mom="hermoine",
     daughter= "penny", son="Zorro";
 document.write(Dad);
 
-document.write(3+3);
+var blues = "i have the blues.";
 
-function My_First_Function () {
-    var str = "this text is green!";
-    var result = str.fontcolor("green");
-    document.getElementById("Green_Text").innerHTML = result;
-}
+document.write(blues);
---
+++ Basic Javascript projects/JS/Basic_Javascript_5.js
@@ -0,0 +1,49 @@
+document.write(typeof "Word");
+
+
+document.write(-3E310) ;
+
+document.write(10 > 2);
+
+document.write(10 < 2);
+
+console.log(2 + 2);
+
+document.write("10" + 5);
+
+document.write(10 == 10);
+
+document.write(3 == 11);
+
+X = 10; 
+Y = 10;
+document.write (X === Y);
+
+X = 82; 
+Y = "82";
+document.write(X === Y);
+
+X = "Magnus";
+B = "Magnus";
+document.write(A === B);
+
+document.write(5 > 2 && 10 > 4);
+
+document.write(5 > 10 && 10 > 4);
+
+document.write(5 > 10 || 10 > 4 );
+
+document.write(5> 10 || 10 > 2);
+
+function my_Function() {
+    document.getElementById("Test").innerHTML = 0/0;
+
+}
+
+function my_Function2() {
+    document.getElementById("Test2").innerHTML = isNAN('This is a string');
+}
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = ! (20 > 10);
+}
