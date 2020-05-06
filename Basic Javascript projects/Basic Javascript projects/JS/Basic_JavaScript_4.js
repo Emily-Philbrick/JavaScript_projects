5dac20f0330729fba69126768888f85b98347ef4
Emily-Philbrick <63964311+Emily-Philbrick@users.noreply.github.com>
2020-05-06 08:03:58 -0600

    help please
    There is something not working right with this code. could someone please assit?

---
+++ Basic Javascript projects/Basic Javascript projects/JS/Basic_JavaScript_4.js
@@ -0,0 +1,10 @@
+function my_Dictionary() {
+    var Animal = {
+        Species: "Dog",
+        Color: "Black"
+        Breed: "Labordor",
+        Age: 5, 
+        Sound: "Bark!"
+    };
+    document.getElementById("Dictionary").innerHTML = Animal.sound; 
+}
---
+++ Basic Javascript projects/Basic Javascript projects/JS/Basic_Javascript_1.js
@@ -0,0 +1,32 @@
+
+
+document.write('Hello,world!');
+
+
+var A = "Hello";
+window.alert(A); 
+
+
+
+document.write("Lisa told Bart, \"Knock it off, Bart!");
+
+
+
+document.write("\Be who you are and say what you feel,"
++"because those who mind don\'t mattter.\""
++"-Dr. Seuss");
+
+
+
+
+var B ="I said" + "Step aside"
+var blues = fontcolor("blue");
+document.write("B , blues"); 
+
+var Family= "the Arezzinis", Dad= "Jeremiah", mom="hermoine",
+    daughter= "penny", son="Zorro";
+document.write(Dad);
+
+var blues = "i have the blues.";
+
+document.write(blues);
